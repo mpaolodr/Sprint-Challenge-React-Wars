821a5e3675db64317aee00506c622de14ab66788
mpaolodr <mpaolodr_1504@yahoo.com>
2019-12-13 11:24:52 -0500

    setup my App.js and began creating CardContainer component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+//Import Components
+import CardContainer from "./components/CardContainer.js";
+
+//Import styles
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  // const [data, setData] = useState([]);
+  const [charArr, setCharArr] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/?page=1")
+      .then(res => {
+        console.log(res.data.results);
+        // setData(res.data);
+        setChar(res.data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CardContainer charArr={charArr} />
     </div>
   );
-}
+};
 
 export default App;
